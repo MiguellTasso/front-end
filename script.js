887534d94f28239adb3c0d61a3d05e974d454ecc
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-29 09:14:09 -0300

    adicionando checkpoint

---
+++ script.js
@@ -0,0 +1,25 @@
+let nome = "miguel";
+let idade = 19;
+let casado = true;
+let valorProduto = 10.50;
+let times = ["Palmeiras", "São Paulo", "Santos"];
+
+
+let num = 2;
+let num1 = 4;
+let soma = num + num1;
+let subtracao = num - num1;
+let multiplicacao = num * num1;
+let divisao = num / num1;
+
+
+console.log(nome);
+console.log(idade);
+console.log(casado);
+console.log(valorProduto);
+console.log(times);
+console.log(soma);
+console.log(subtracao);
+console.log(multiplicacao);
+console.log(divisao);
+
